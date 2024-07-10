b28d060ed18b7e84d3efeece1f6ab52f5ff2706d
Germán Castro <germancastromontes@gmail.com>
2024-07-10 21:13:53 +0200

    feat: adding ttl for kv storage

--- src/lib/storage/kv.ts
+++ src/lib/storage/kv.ts
@@ -7,6 +7,7 @@ const storage = createStorage({
   driver: vercelKVDriver({
     url: process.env.UPSTASH_REDIS_REST_URL,
     token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    ttl: 60 * 60 * 24 // 24 hours
   }),
 });
 
